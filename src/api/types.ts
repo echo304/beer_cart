a6af050cb7b1b4a53a5ed295661f62413eb08249
Sangboak Lee <echo3042@gmail.com>
2018-09-11 22:10:35 +0900

    add types for API

---
+++ src/api/types.ts
@@ -0,0 +1,28 @@
+interface Tag {
+  key: string;
+  name: string;
+}
+
+export interface Beer {
+  id: number;
+  name: string;
+  image: string;
+  tags: Tag[];
+  price: number;
+  stock: number;
+}
+
+export interface Purchase {
+  id: number;
+  count: number;
+}
+
+export interface PurchaseSuccess {
+  totalCount: number;
+  totalPrice: number;
+}
+
+export interface Error {
+  title: string;
+  reason: string;
+}
