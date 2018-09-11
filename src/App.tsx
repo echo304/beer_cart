2c6034111eac2b4d03b03510a113aa7700754d4d
Sangboak Lee <echo3042@gmail.com>
2018-09-11 22:13:24 +0900

    apply icon for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
-import { Link, Route } from 'react-router-dom';
+import {
+  NavLink,
+  Redirect,
+  Route,
+  RouteComponentProps,
+  Switch,
+  withRouter
+} from 'react-router-dom';
+
+import CartSelectedSvg from '../assets/img-cart-selected.svg';
+import CartSvg from '../assets/img-cart.svg';
+import ListSelectedSvg from '../assets/img-list-selected.svg';
+import ListSvg from '../assets/img-list.svg';
 
 import routes from './routes';
 
-class App extends React.Component {
+class App extends React.Component<RouteComponentProps<any>> {
   public render() {
+    const { location } = this.props;
+    const isOnList = location.pathname === '/list';
+    const isOnCart = location.pathname === '/cart';
     return (
       <div>
         <nav>
-          <Link to="/list">list</Link>
-          <Link to="/cart">cart</Link>
+          <NavLink to="/list">
+            {isOnList ? <img src={ListSelectedSvg} /> : <img src={ListSvg} />}
+          </NavLink>
+          <NavLink to="/cart">
+            {isOnCart ? <img src={CartSelectedSvg} /> : <img src={CartSvg} />}
+          </NavLink>
         </nav>
         <div>
-          {routes.map((route) => (
-            <Route path={route.path} component={route.component} key={route.path} />
-          ))}
+          <Switch>
+            {routes.map((route) => (
+              <Route path={route.path} component={route.component} key={route.path} />
+            ))}
+            <Redirect to="/list" />
+          </Switch>
         </div>
       </div>
     );
   }
 }
 
-export default hot(module)(App);
+export default hot(module)(withRouter(App));
