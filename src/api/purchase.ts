e547080c7cd94e1f2ad03ce8f300747b3b72eff5
Sangboak Lee <echo3042@gmail.com>
2018-09-12 19:15:12 +0900

    change purchase API
    - stubbing

--- src/api/purchase.ts
+++ src/api/purchase.ts
@@ -1,9 +1,12 @@
 import { Purchase, PurchaseSuccess } from './types';
-import { apiPost } from './utils';
+// import { apiPost } from './utils';
 
 export async function post(data: Purchase[]): Promise<PurchaseSuccess> {
-  const url = `http://localhost:3004/purchases`;
-  await apiPost(url, data);
+  // Since Mock API server doesn't support POST purchase operation
+  // API call is commented out
+  //
+  // const url = `http://localhost:3004/purchases`;
+  // await apiPost(url, data);
 
   // This is mock data
   return {
