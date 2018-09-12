072624f9e90c5451144bfbe9f892facf58a2e2fa
Sangboak Lee <echo3042@gmail.com>
2018-09-12 20:22:30 +0900

    remove production store config

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -2,10 +2,6 @@ import { Store } from 'redux';
 
 let configureStore: () => Store<any>;
 
-if (process.env.NODE_ENV === 'production') {
-  configureStore = require('./configureStore.prod').default;
-} else {
-  configureStore = require('./configureStore.dev').default;
-}
+configureStore = require('./configureStore.dev').default;
 
 export default configureStore;
