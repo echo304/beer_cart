f02a0964315c609def136f467a7c669f60299e97
Sangboak Lee <echo3042@gmail.com>
2018-09-12 11:47:43 +0900

    render more logic

--- src/containers/BeerListContainer/index.tsx
+++ src/containers/BeerListContainer/index.tsx
@@ -1,16 +1,24 @@
+import autobind from 'autobind-decorator';
 import _ from 'lodash';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import styled from 'styled-components';
 
 import { Beer } from '../../api/types';
+import Button from '../../components/Button';
+import { RENDER_COUNT_PER_REQUEST } from '../../lib/constants';
 import BeerListActions from '../../redux/beerList/actions';
 import { RootState } from '../../redux/types';
 
 import ItemCard from './ItemCard';
 
+const RenderMoreButtonWrapper = styled.div`
+  text-align: center;
+`;
 interface BeerListContainerProps {
   beersArray: Beer[];
+  hasMoreItemToRender: boolean;
   beerListBoundActions: typeof BeerListActions;
 }
 
@@ -20,12 +28,17 @@ class BeerListContainer extends React.Component<BeerListContainerProps> {
   }
 
   public render() {
-    const { beersArray } = this.props;
+    const { beersArray, hasMoreItemToRender } = this.props;
     return (
       <div>
         {beersArray.map((beer) => (
           <ItemCard key={beer.id} {...beer} />
         ))}
+        {hasMoreItemToRender && (
+          <RenderMoreButtonWrapper>
+            <Button label="더보기 +" onClick={this.handleRenderMoreClick} />
+          </RenderMoreButtonWrapper>
+        )}
       </div>
     );
   }
@@ -34,18 +47,26 @@ class BeerListContainer extends React.Component<BeerListContainerProps> {
     const { beerListBoundActions } = this.props;
     beerListBoundActions.fetchBeers();
   }
+
+  @autobind
+  private handleRenderMoreClick() {
+    const { beerListBoundActions } = this.props;
+    beerListBoundActions.renderBeers(RENDER_COUNT_PER_REQUEST);
+  }
 }
 
 const mapStateToProps = (state: RootState) => {
   const { beerList } = state;
   const { cursor } = beerList;
+  const hasMoreItemToRender = cursor < (beerList.beerIds.length as number);
   const beersArray = _(beerList.beerIds)
     .map((id) => beerList.beers[id])
     .take(cursor)
     .value();
 
   return {
-    beersArray
+    beersArray,
+    hasMoreItemToRender
   };
 };
 
